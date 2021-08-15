22dbf3dd2d24b77c208d4750edfc3ca414a2d708
Katherine Pitts <kvp06933@gmail.com>
2021-08-15 17:14:52 -0500

    Updated app.html with working code

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello world");
+
+//get values -- start or controller function
+function getValues(){
+    document.getElementById("alert").classList.add("invisible");
+    let str = document.getElementById("stringValue").value;
+    let revString = reverseString(str);
+    displayReverseString(revString);
+    
+    
+}
+
+//reverse a string -- logic function
+function reverseString(str){
+let revString =[];
+
+//reverse a string using a for loop
+for (let index = str.length-1; index >= 0; index--) {
+    revString += str[index];
+     
+}
+return revString;
+
+}
+
+//display the reversed string to the user -- view function
+function displayReverseString(revString){
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+    document.getElementById("alert").classList.add("show");
+    
+
 }
